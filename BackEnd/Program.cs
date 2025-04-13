440d828fb3b9893588933e7193c74b87ab711e8a
jPizarro10280 <jpizarro10280@ufide.ac.cr>
2025-04-12 18:53:13 -0600

    creacion de migracion

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -29,11 +29,11 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddDefaultTokenProviders();
 builder.Services.Configure<IdentityOptions>(options => 
 {
-    options.Password.RequireDigit = true;
-    options.Password.RequireLowercase = true;
-    options.Password.RequireNonAlphanumeric = true;
-    options.Password.RequireUppercase = true;
-    options.Password.RequiredLength = 8;
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 3;
     
 });
 
